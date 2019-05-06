d8322544efc05d8b672ec8c02723d634dfb727d0
郭文武 <guowenwu1981815@163.com>
2019-05-07 00:09:17 +0800

    初始化

---
+++ mvcCookieAuthSample2/Config.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+namespace mvcCookieAuthSample
+{
+    public class Config
+    {
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client> { new Client() {
+                 ClientId="mvc",
+                 ClientSecrets = { new Secret("secrect".Sha256()) },
+                 AllowedGrantTypes = GrantTypes.Implicit,
+                 AllowedScopes = {"api1" }
+            } };
+        }
+
+        public static IEnumerable<ApiResource> GetResources()
+        {
+            return new List<ApiResource> { new ApiResource("api1", "API Application") };
+        }
+
+        public static List<TestUser> GetTestUsers()
+        {
+            return new List<TestUser> { new TestUser {
+                SubjectId ="10000",
+                Username="gww",
+                Password="password"
+            } };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile()
+            };
+        }
+    }
+}
